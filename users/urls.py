b74cab49d98cf402eca2d12bad5bbb4650576dc3
Jotham Okoye <37877230+jothamc@users.noreply.github.com>
2019-12-14 15:33:23 +0100

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import RegisterView,Suggest
+from .views import RegisterView,Suggest,Results
 
 app_name = 'user'
 urlpatterns = [
     # path("",IndexPage,name="profile"),
     path('register/',RegisterView,name="register"),
     path('',include("django.contrib.auth.urls")),
-    path("suggest/",Suggest,name="suggest")
+    path("profile/",Suggest,name="suggest"),
+    path("results/",Results,name="results")
 ]
--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ from .models import User
 
 # Create your views here.
 #Have views/models that handle Clients and Riders different or use the User model with the 'is_rider' field?
-class HandyUserCreationForm(UserCreationForm):
+class UserCreationForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = UserCreationForm.Meta.fields + ('email',)
@@ -36,36 +36,37 @@ def RegisterView(request):
 	return render(request,"registration/register.html",{"form":form})
 
 
-# class SuggestDetailView(DetailView):
-# 		model = Suggest
-# 		template_name = "suggest.html"
-# )
-
-
 from django import forms
-from crawler import courses
+import json
 
+courses_json = open("courses.json")
+courses = json.loads( courses_json.read() )
+courses_json.close()
 all_courses = []
 count = 0
 
 for i in courses:
 	all_courses.append( tuple([count,i]) )
 	count = count + 1
+all_courses.sort()
+
+ssce_dict = {
+	"A1":1, "B2":2,	"B3":3,
+	"C4":4,	"C5":5,	"C6":6,
+	"D7":7,	"E8":8,	"F9":9,
+}
 
 class suggestForm(forms.Form):
 	"""suggestForm definition."""
 
-	SSCE = (
-		("A1","A1"),
-		("B2","B2"),
-		("B3","B3"),
-		("C4","C4"),
-		("C5","C5"),
-		("C6","C6"),
-		("D7","D7"),
-		("E8","E8"),
-		("F9","F9"),
-	)
+	def ssce_tuple(ssce_list):
+		all_ssce = []
+		for i,j in ssce_list.items():
+			all_ssce.append(tuple([j,i]))
+		return tuple(all_ssce)
+
+	SSCE = ssce_tuple(ssce_dict)
+
 	course = forms.ChoiceField(choices=(all_courses))
 	subject_1 = forms.IntegerField(max_value=100,min_value=0,label="Use of English")
 	subject_2 = forms.IntegerField(max_value=100,min_value=0,)
@@ -87,4 +88,50 @@ class suggestForm(forms.Form):
 def Suggest(request):
 	if request.method == "GET":
 		form = suggestForm()
-	return render(request,"suggest.html",{"form":form})
+		return render(request,"profile.html",{"form":form})
+	elif request.method == "POST":
+		form = suggestForm(request.POST)
+		return Results(request)
+		# return render(request,"results.html",{"form":form})
+
+
+def Results(request):
+	post = request.POST
+	course = all_courses[int(post["course"])]
+	chosen_course = course[1]
+	english = int(post['subject_1'])
+	subject_2 = int( post['subject_2'])
+	subject_3 = int( post['subject_3'])
+	subject_4 = int( post['subject_4'])
+	total_jamb_score = english + subject_2 + subject_3 + subject_4
+	results = {
+		"Chosen course": chosen_course,
+		"Use of English": english,
+		"JAMB Subject 2": subject_2,
+		"JAMB Subject 3": subject_3,
+		"JAMB Subject 4": subject_4,
+		"Total JAMB Score": total_jamb_score,
+		"SSCE 1":post["ssce_1"],
+		"SSCE2":post["ssce_2"],
+		"SSCE 3":post["ssce_3"],
+		"SSCE 4":post["ssce_4"],
+		"SSCCE 5":post["ssce_5"],
+		"SSCE 6":post["ssce_6"],
+		"SSCE 7":post["ssce_7"],
+		"SSCE 8":post["ssce_8"],
+		}
+	return render(request,"results.html",{"results":results})
+
+def jamb_score_check(jamb_score,school,course):
+	if jamb_score >= school.course.cut_off_score:
+		return True
+	return False
+
+def ssce_score_check(ssce_scores,school,course):
+	scores = {}
+	for subject in ssce_scores:
+		if subject in school.course.ssce_subjects and subject.score > school.course.ssce_subjects[subject].score:
+			scores[subject] = True
+		else:
+			scores[subject] = False
+	return scores
